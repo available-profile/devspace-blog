e87fc1806506ab256ed65a82292532c51783102c
Lynde Bayona <lynde.bayona@erfinderstore.com>
2021-06-11 17:14:33 +0800

    Section 12: Pagination & Category Pages - #67. Pagination Links Component

---
+++ pages/blog/page/[page_index].js
@@ -0,0 +1,96 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import Link from 'next/link'
+import Layout from '../../../components/Layout'
+import Post from '../../../components/Post'
+import Pagination from '../../../components/Pagination'
+import { sortByDate } from '../../../utils'
+import { POSTS_PER_PAGE } from '../../../config'
+
+export default function BlogPage({posts, numPages, currentPage}) {
+  //console.log(posts)
+  return (
+    <Layout>
+      <h1 className="text-5xl border-b-4 p-5 font-bold">Blog</h1>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
+
+      <Pagination currentPage={currentPage} numPages={numPages}/>
+      
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+
+  const files = fs.readdirSync(path.join('posts'))
+  const numPages = Math.ceil(files.length / POSTS_PER_PAGE)
+  
+  let paths = []
+
+  for(let i = 1; i <= numPages; i++) {
+    paths.push({
+      params: {page_index: i.toString()}
+    })
+  }
+
+  return {
+    paths,
+    fallback: false
+  }
+
+}
+
+export async function getStaticProps({params}) {
+
+  const page = parseInt((params && params.page_index) || 1)
+
+  const files = fs.readdirSync(path.join('posts'))
+  //console.log(files)
+  //outputs files object with array of md files in posts folder
+  // [
+  //   'django-crash-course.md',
+  //   'javascript-performance-tips.md',
+  //   'new-in-php-8.md',
+  //   'python-book-review.md',
+  //   'react-crash-course.md',
+  //   'tailwind-vs-bootstrap.md',
+  //   'writing-great-unit-tests.md'
+  // ]
+
+  // map thru the files array
+  const posts = files.map(filename => {
+    const slug = filename.replace('.md', '')
+
+    // parse into an object
+    const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8')
+
+
+    const {data:frontmatter} = matter(markdownWithMeta)
+
+    return {
+      slug,
+      frontmatter
+    }
+  })
+
+  const numPages = Math.ceil(files.length / POSTS_PER_PAGE)
+  const pageIndex = page - 1
+  const orderedPosts = posts
+    .sort(sortByDate)
+    .slice(pageIndex * POSTS_PER_PAGE, (pageIndex + 1) * POSTS_PER_PAGE)
+
+  return {
+    props: {
+      /* sort by recent date and display only 6 posts */
+      posts: orderedPosts,
+      numPages,
+      currentPage: page
+    },
+  }
+}
